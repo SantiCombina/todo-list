f7534ab1cba773b5f5ce0ef576e8913f45fb08c2
SantiCombina <santicombina@gmail.com>
2024-04-24 18:05:31 -0300

    Added react router

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,16 @@ import "./assets/fontawesome/css/all.min.css";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import {Toaster} from "sonner";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+            <Toaster richColors />
+        </React.StrictMode>
+    </BrowserRouter>,
 );

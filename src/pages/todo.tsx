0f1b8bd1fbde3855da85f3c9ddc615e2f867e2f5
SantiCombina <santicombina@gmail.com>
2024-05-03 00:59:59 -0300

    Remove extra import from todo component

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -2,7 +2,6 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {useEffect, useRef, useState} from "react";
 import {useForm} from "react-hook-form";
 import {toast} from "sonner";
-import {useNavigate} from "react-router-dom";
 
 import {supabase} from "../supabase/supabase";
 
